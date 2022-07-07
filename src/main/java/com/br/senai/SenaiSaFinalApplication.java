b37b0da6c9a548d24fd9686adeb4da64fe036021
hugoodesa <hugoodesa@gmail.com>
2022-07-07 20:40:00 -0300

    Initial commit

---
+++ src/main/java/com/br/senai/SenaiSaFinalApplication.java
@@ -0,0 +1,13 @@
+package com.br.senai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaiSaFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenaiSaFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/senai/SenaiSaFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.senai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenaiSaFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
